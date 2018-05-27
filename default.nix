7ecb7c7a4b8cbf015d72bdac8e030125c51be6b4
Thibault Polge <thibault@thb.lt>
2018-05-27 16:28:24 +0200

    Update Nix package

--- default.nix
+++ default.nix
@@ -1,12 +1,25 @@
 with import <nixpkgs>{};
 
 { nixpkgs ? import <nixpkgs> {} }:
-stdenv.mkDerivation {
-  name = "uberwald-0.0.0";
-  src = fetchgit {
-    url = "https://github.com/thblt/uberwald";
-    sha256 = "1wy645v6cy9pns4gxqb6vb939fd2smp8hhzfsdv43c5yipn1nqmf";
-  };
+stdenv.mkDerivation rec {
+  version = "0.0.0";
+  name = "uberwald-${version}";
 
-  buildInputs = with pkgs; [readline];
-}
+  # src = fetchgit {
+  #   url = "https://github.com/thblt/uberwald";
+  #   sha256 = "1wy645v6cy9pns4gxqb6vb939fd2smp8hhzfsdv43c5yipn1nqmf";
+  # };
+
+  buildInputs = with pkgs; [
+                cmake
+                readline
+                llvmPackages.clang.cc
+                doxygen
+                check
+                bear
+                ctags
+                python36Packages.sphinx
+                python36Packages.breathe
+                python36Packages.sphinx_rtd_theme
+                lldb ];
+                }
